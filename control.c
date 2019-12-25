33e6e9effff12b9d4baa4653dfab9f1546160305
Brandon <bchen00@stuy.edu>
2019-12-25 03:50:19 +0000

    worked on create

--- control.c
+++ control.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <errno.h>
 
+#define SEM_KEY 24602
+#define SH_KEY 24601
+
 union semun{
   int val;
   struct semid_ds *buf;
@@ -12,15 +15,34 @@ union semun{
 
 int main(int argc, char* argv[]){
   if (argc > 1){
-    if (-c){
-      create
+    if (strcmp(argv[1], "-c") == 0){
+      create()
     }
-    else if (-r){
-      read
+    else if (strcmp(argv[1], "-r") == 0){
+      read()
     }
-    else if (-v){
-      view
+    else if (strcmp(argv[1], "-v") == 0){
+      view()
     }
   }
   return 0;
+}
+
+void create(){
+  int send = semget(SEM_KEY,1,IPC_CREAT | 0644);
+
+  union semun se;
+  semctl(send, 0, SETVAL, se);
+  
+  shmd = shmget(SH_KET,1,IPC_CREAT | 0644);
+
+  fd = open("file.txt", O_CREAT | O_RDWR, 0644);
+}
+
+void read(){
+
+}
+
+void view(){
+
 }
